7120e839f690840d9e7abc54d812d05a4d657441
AnandaAp <ananda.apriliansyah@ti.ukdw.ac.id>
2021-07-29 13:33:10 +0700

    Soal 4 Done

---
+++ Soal4/app/src/main/java/com/ananda/soal4/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ananda.soal4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ananda.soal4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        revereseProcess()
+        setContentView(binding.root)
+    }
+
+    private fun revereseProcess() {
+        binding.confirmBtn.setOnClickListener{
+            val case = binding.input.text.toString()
+            if(case != "" || case != " "){
+                val reverse = case.reversed()
+                binding.output.text = reverse
+                println(reverse)
+            }
+        }
+    }
+}
